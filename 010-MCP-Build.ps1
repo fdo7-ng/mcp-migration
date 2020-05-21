9cf63714415009ac1252a093442036b94839dde1
fdo ng <fernando.ng@cis.ntt.com>
2020-05-21 20:27:06 +0000

    add ps file

---
+++ 010-MCP-Build.ps1
@@ -0,0 +1,23 @@
+
+$location = 'eastus'
+
+$cnd_file = "./output/cloud_network_domains.json"
+$cnd_data = Get-Content $cnd_file | ConvertFrom-JSON 
+
+
+foreach ($itm in $cnd_data.data.network_domain){
+
+    Write-host  "$($itm.Name)"
+    Write-host  "$($itm.description)"
+
+    $rgName = "rg_" + $itm.Name
+
+    # Create a resource group.
+    Write-Host "Creating Resource Group [ $rgName]" -ForegroundColor Magenta
+    if (Get-AzResourceGroup -Name $rgName -ErrorAction SilentlyContinue){
+        Write-Host "`t[ $rgName] resource group found" -ForegroundColor Yellow
+    }else{
+        Write-Host "`t[ $rgName] creating resource group" -ForegroundColor Green
+        New-AzResourceGroup -Name $rgName -Location $location
+}
+}
