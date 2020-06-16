1d15386a5b27889ad2e0e46c947b694dc699f204
fdo ng <fernando.ng@cis.ntt.com>
2020-06-16 21:05:02 +0000

    added 020 script to create vnet and subnets

--- 020-MCP-VNet-Build.ps1
+++ 020-MCP-VNet-Build.ps1
@@ -1,6 +1,129 @@
 
-$vnet_file = './output/vlans_SAPDRS_POC.json'
+<#
+    Description: Script to process json file with vlan information generated from ansible report 
+        - Script will generate vlan object
+        - determine the address space it should belong to for VNET creation
+        - determine parameters required to create subnet
+    Assumption:
+        - Currently assuming Network Domain will be equivalent to Resource Group in Azure
+        - CC Vlans if /24, this tool will create a /16 address
+        - All server vlans will be created under same VNET. 
+        - vNet will be named after the CND name
+    Considerations:
+        - In CC we create server vlans using /24, we can create /24 size VNET/Subnet. 
+        or 
+        - Create a /16 address for each corresponding /24 server vlan.
+    Updates:
+    06/16/2020: Added vNet Creation and Subnet creation
+        - vNet and subnet name cannot contains spaces, replaced spaces " " with underscore "_"
+#>
 
-$vnet_data = get-content $vnet_file | ConvertFrom-Json
 
-$vnet_data.data.vlan
+$file = '.\output\vlans\vlans_ECL-MCP migration.json'
+$rg_name = 'fdpsrg01'
+$location = 'eastus'
+
+
+
+$json_data = Get-Content $file | ConvertFrom-Json
+
+$vlan_list = $json_data.data.vlan
+
+$rpt = @()
+
+foreach ($vlan in $vlan_list){
+
+    Write-Host "Processing on " $vlan.Name
+    $vlan_obj = $vlan | select Name, datacenterId, ipv4GatewayAddress, Ipv6GatewayAddress, 
+        networkDomain, privateIpv4Range
+    
+    $vnet_name = $vlan.networkDomain.name.replace(" ","_")
+    $subnetprefix = $vlan_obj.privateIpv4Range.address +"/"+ $vlan_obj.privateIpv4Range.prefixSize
+    if ( $vlan.privateIpv4Range.prefixSize -eq 24 ){
+        $address_split = $vlan_obj.privateIpv4Range.address.split('.')
+        $address_split[2] = '0'
+        $address_split[3] = '0'
+        $address_space = $address_split -join '.'
+        $addressprefix = $address_space + "/16"
+    }
+
+    
+    $vlan_obj | Add-Member NoteProperty -Name addressprefix -Value $addressprefix # to create vNET
+    $vlan_obj | Add-Member NoteProperty -Name subnetprefix -Value $subnetprefix # to create subnet
+    $vlan_obj | Add-Member NoteProperty -Name ResourceGroup -Value $rg_name
+
+    Write-Host "`t --- resource group name: $rg_name"
+    Write-Host "`t --- vNet Name: $vnet_name"
+    Write-Host "`t --- Address Space: $addressprefix"
+        
+    $vlan_obj
+
+
+    $vNet = Get-AzVirtualNetwork -ResourceGroupName $rg_name -Name $vnet_name -ErrorAction SilentlyContinue
+    #$vnet = $true
+    if ($vNet){
+        Write-Host "Adding Subnet [ $subnetname] to Vnet [$vnet_name] " -yellow
+
+        # Verify if address space exist and create if not
+        if (! $vNet.AddressSpace.AddressPrefixes.contains($addressprefix)){
+            Write-Host "Creating Address Prefix [$addressprefix]"
+            $vNet.AddressSpace.AddressPrefixes.add($addressprefix)
+            Set-AzureRmVirtualNetwork -VirtualNetwork $vNet   
+            $vNet = Get-AzVirtualNetwork -ResourceGroupName $rg_name -Name $vnet_name -ErrorAction SilentlyContinue
+        }
+
+        $subnetname = $vlan_obj.name.replace(" ","_")
+
+        $subnet = Get-AzVirtualNetworkSubnetConfig -VirtualNetwork $vnet -Name $subnetname -ErrorAction SilentlyContinue
+
+        if (! $subnet) {
+            Add-AzVirtualNetworkSubnetConfig `
+                -Name $subnetname `
+                -AddressPrefix $subnetprefix `
+                -VirtualNetwork $vNet
+            Set-AzVirtualNetwork -VirtualNetwork $vNet
+        # $cmd = "Add-AzVirtualNetworkSubnetConfig `
+        #           -Name $subnetname `
+        #           -AddressPrefix $subnetprefix `
+        #           -VirtualNetwork $virtualNetwork"
+        # Write-Host "Add Subnet: `n`t$cmd" -ForegroundColor Yellow
+        # Invoke-Expression $cmd
+
+        }
+    }else{
+        # Only run if VNET Does not exist
+        Write-Host "Creating VNET : $vnet_name" -ForegroundColor Yellow  
+        
+
+        #Write-Host "Create VNET: `n`t$cmd"   -ForegroundColor Yellow     
+        #Invoke-Expression $cmd
+    
+        ## before actually running this need to have the $VNET inside $virtualNetwork
+        $subnetname = $vlan_obj.name
+        # $cmd = "Add-AzVirtualNetworkSubnetConfig `
+        #           -Name $subnetname `
+        #           -AddressPrefix $subnetprefix `
+        #           -VirtualNetwork $virtualNetwork"
+        # Write-Host "Add Subnet: `n`t$cmd" -ForegroundColor Yellow
+        # Invoke-Expression $cmd
+        $subnet = New-AzVirtualNetworkSubnetConfig `
+            -Name $subnetname `
+            -AddressPrefix $subnetprefix `
+            -VirtualNetwork $vNet
+
+        $vNet = New-AzVirtualNetwork `
+            -ResourceGroupName $rg_name `
+            -Location EastUS `
+            -Name $vnet_name `
+            -AddressPrefix $addressprefix `
+            -Subnet $subnet
+
+    }
+    
+
+    $rpt += $vlan_obj    
+}
+
+
+#Write-Host "`t`t ---- Show Full Report ---" -ForegroundColor Green
+#$rpt 
--- 030-MCP-VNetInterface_Create.ps1
+++ 030-MCP-VNetInterface_Create.ps1
@@ -1,3 +1,18 @@
+<#
+    Powershell Script to Create Network Interface
+
+    Assumption:
+    - Vnet and Subnet are already created and exist
+    - All VMs in the list belong to the same vNet / Subnet
+    - Accepts server.json file exported from Ansible MCP Module
+
+    Updates:
+    - 06/16/2020: Initial Version
+#>
+
+
+## Parameter Section
+
 $location = 'eastus'
 $networkdomainID = 'd6f0740b-9026-4222-a0a5-f21e37c2a767'
 $resourcegroup = 'fdpsrg01'
@@ -6,9 +21,13 @@ $subnet_name = 'SAPDRSPOC_VLAN'
 
 $server_file = './output/servers.json'
 $server_data = Get-Content $server_file | ConvertFrom-JSON 
-
 $server_count = $server_data.data.count
 $server_list = $server_data.data.server
+
+# Configure destroy parameter to delete network interface created by this script ($true|$false)
+$destroy = $true
+
+# Code Section
 
 $vnet = Get-AzureRmVirtualNetwork -Name $vnet_name
 
@@ -26,12 +45,18 @@ foreach ($vm in $server_list){
     $network_interface = Get-AzNetworkInterface -ResourceGroupName $resourcegroup -Name $vmNicName -ErrorAction SilentlyContinue
 
     if($network_interface) {
-
+        Write-Host " --- Found network interface - " $vmNicName -ForegroundColor Green
 
     }else{
-        Write-Host "Creating network interface - " $vmNicName
+        Write-Host " --- Creating network interface - " $vmNicName -ForegroundColor Yellow
+        $subnetID = (Get-AzureRmVirtualNetworkSubnetConfig -Name 'SAPDRSPOC_VLAN' -VirtualNetwork $vnet).id
 
+        New-AzureRmNetworkInterface -Name $vmNicName -ResourceGroupName $resourcegroup -Location $location -SubnetId $subnetID -PrivateIpAddress $privateIpv4 | Out-Null
+    }
 
+    if ($destroy){
+        Write-Host " --- Destroy network interface - "  $vmNicName -ForegroundColor Red
+        $network_interface | Remove-AzNetworkInterface -Force -Confirm:$false
     }
 
 }
